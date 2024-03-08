46f98e55d732af652badb864e1e09e63b3ddb72a
andrew-zaborenko <72259060+andrew-zaborenko@users.noreply.github.com>
2024-03-08 15:40:35 +0300

    Update get_metrics.py

--- get_metrics.py
+++ get_metrics.py
@@ -232,7 +232,7 @@ def plot_ced_2_predictors(errors_1, errors_2, threshold, name_1='Errors 1', name
     plt.grid(True)
     plt.xlim(0, threshold)
     plt.legend()
-    fig.savefig('CED_plot.png')
+    plt.savefig('CED_plot.png')
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Face Landmarks Detection Inference")
